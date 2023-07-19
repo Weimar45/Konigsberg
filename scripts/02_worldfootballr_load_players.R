7fbd2bf50e009dc01b0d55e5902ce10d687e16ff
Alex42NGNAR <alejandronavasgonzalez6@gmail.com>
2023-07-20 00:21:18 +0200

    Metadata del script añadido

--- scripts/02_worldfootballr_load_players.R
+++ scripts/02_worldfootballr_load_players.R
@@ -1,3 +1,12 @@
+# ----- Descripción del Script ----
+#
+# Ejemplo de descarga de datos en crudo de los jugadores
+# Fecha: 20/07/2023
+# Última vez editado por: Alejandro Navas González
+#
+
+# ------ Load packages ------
+
 library(tidyverse)
 library(worldfootballR)
 library(openxlsx)
@@ -11,9 +20,9 @@ leah_williamson_passing <-  fb_player_season_stats(
 glimpse(leah_williamson_passing)
 
 wb <- createWorkbook("Konigsberg")
-addWorksheet(wb, sheetName = "")
-writeData(wb, sheet = "", tudataframe)
-saveWorkbook(wb, file = "results/....xlsx", overwrite = TRUE)
+addWorksheet(wb, sheetName = "Leah Williamson Passing")
+writeData(wb, sheet = "Leah Williamson Passing", leah_williamson_passing)
+saveWorkbook(wb, file = "results/leah_williamson_passing.xlsx", overwrite = TRUE)
 
 big5_player_possession <- fb_big5_advanced_season_stats(
   season_end_year = 2023, 
@@ -24,9 +33,9 @@ big5_player_possession <- fb_big5_advanced_season_stats(
 glimpse(big5_player_possession)
 
 wb <- createWorkbook("Konigsberg")
-addWorksheet(wb, sheetName = "")
-writeData(wb, sheet = "", tudataframe)
-saveWorkbook(wb, file = "results/....xlsx", overwrite = TRUE)
+addWorksheet(wb, sheetName = "Big5 Player Possession")
+writeData(wb, sheet = "Big5 Player Possession", big5_player_possession)
+saveWorkbook(wb, file = "results/big5_player_possession.xlsx", overwrite = TRUE)
 
 leah_williamson_goal_log <- fb_player_goal_logs(
   player_urls = leah_williamson, 
